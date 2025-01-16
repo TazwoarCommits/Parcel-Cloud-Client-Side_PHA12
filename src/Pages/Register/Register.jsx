4014d84db2dc20891a9bf81e20bd270e741611be
TazwoarCommits <tazwoarlives@gmail.com>
2025-01-16 22:18:01 +0600

    fixed code in registration page and inserted new users in db

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -6,39 +6,59 @@ import useAuth from "../../Hooks/useAuth";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
-import { useQuery } from "@tanstack/react-query";
 
 
 const Register = () => {
-    const { createUser , updateUsersProfile } = useAuth();
-    const {handleSubmit , register , formState: { errors } } = useForm() ;
-    const axiosPublic = useAxiosPublic() ;
-    const navigate = useNavigate() ;
+    const { createUser, updateUsersProfile, user } = useAuth();
+    const { handleSubmit, register, formState: { errors } } = useForm();
+    const axiosPublic = useAxiosPublic();
+    const navigate = useNavigate();
 
-    const image_hosting_key = import.meta.env.VITE_image_hosting_API_key ; 
-    const image_hosting_API = `https://api.imgbb.com/1/upload?key=${image_hosting_key}` ;
+    
+
+    const image_hosting_key = import.meta.env.VITE_image_hosting_API_key;
+    const image_hosting_API = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
     const onSubmit = async form => {
-        console.log(form , form.photo[0]);
-        const imageFile = {image : form.photo[0]} ;
-        const {data} = await axiosPublic.post ( image_hosting_API , imageFile , {
-            headers : {
-                "Content-Type" : "multipart/form-data"
-            }
-        }) ;
-        console.log(data.data.display_url);
+        try {
+            console.log("form", form, "photo", form.photo[0]);
+            const imageFile = { image: form.photo[0] };
+            const { data } = await axiosPublic.post(image_hosting_API, imageFile, {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            });
 
-        createUser(form.email , form.password)
-        .then(res => {
-            if(res.user){
-                updateUsersProfile(form.name , form.photo)
-                toast.success("Successfully Registered") ;
-                console.log(res);
-                navigate("/")
+            console.log(data)
+
+            const newUser = {
+                name: form.name,
+                email: form.email,
+                photo: data.data.display_url,
+                role: form.role.toLowerCase(),
             }
-        })
+
+            const userRes = await createUser(form.email, form.password);
+            if (userRes.user) {
+
+                const profileRes = await updateUsersProfile(newUser.name, newUser.photo);
+                console.log(profileRes);
+
+                const { data } = await axiosPublic.post("/users", newUser) ;
+                console.log(data);
+
+                toast.success("Successfully Logged In")
+                navigate("/")
+                console.log(user);
+            }
+        }
+
+        catch(err){
+            console.log(err.message);
+        }
+
     }
-    
+
     return (
         <div>
             <Helmet>
@@ -58,14 +78,14 @@ const Register = () => {
                                     <label className="label">
                                         <span className="label-text">Name</span>
                                     </label>
-                                    <input type="text" {...register("name" , {required : true})}  placeholder="username" className="input input-bordered"/>
+                                    <input type="text" {...register("name", { required: true })} placeholder="username" className="input input-bordered" />
                                     {errors.name && <span className="text-red-700 text-sm">Name is Required</span>}
                                 </div>
                                 <label className="form-control md:w-1/2 max-w-xs">
                                     <div className="label">
                                         <span className="label-text">Register As</span>
                                     </div>
-                                    <select defaultValue="default" {...register("role" , {required : true})} className="select select-bordered">
+                                    <select defaultValue="default" {...register("role", { required: true })} className="select select-bordered">
                                         <option disabled value="default">Pick one</option>
                                         <option>User</option>
                                         <option>Delivery Man</option>
@@ -78,26 +98,27 @@ const Register = () => {
                                     <label className="label">
                                         <span className="label-text">Email</span>
                                     </label>
-                                    <input type="email" {...register("email" , {required : true})} placeholder="email" className="input input-bordered" />
+                                    <input type="email" {...register("email", { required: true })} placeholder="email" className="input input-bordered" />
                                     {errors.email && <span className="text-red-700 text-sm">Email is Required</span>}
                                 </div>
                                 <div className="form-control md:w-1/2">
                                     <label className="label">
                                         <span className="label-text">Password</span>
                                     </label>
-                                    <input type="password" {...register("password" , 
-                                        {required : true , 
-                                        pattern :  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/
+                                    <input type="password" {...register("password",
+                                        {
+                                            required: true,
+                                            pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/
                                         })} placeholder="password" className="input input-bordered" />
-                                     {errors.password?.type === "required" && <span>Password is required</span>}
-                                     {errors.password?.type === "pattern" && <span className="text-red-700">Need 6 characters with one lowercase and uppercase character</span>}
+                                    {errors.password?.type === "required" && <span>Password is required</span>}
+                                    {errors.password?.type === "pattern" && <span className="text-red-700">Need 6 characters with one lowercase and uppercase character</span>}
                                 </div>
                             </div>
                             <label className="form-control w-full">
                                 <div className="label">
                                     <span className="label-text">Your Photo</span>
                                 </div>
-                                <input type="file"  {...register("photo" , {required : true})} className="file-input file-input-bordered w-full max-w-xs" />
+                                <input type="file"  {...register("photo", { required: true })} className="file-input file-input-bordered w-full max-w-xs" />
                                 {errors.photo && <span className="text-red-700 text-sm">Photo is Required</span>}
                             </label>
                             <div className="form-control mt-6">
--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -24,31 +24,19 @@ const Navbar = () => {
                             <span className="badge badge-sm indicator-item">8</span>
                         </div>
                     </div>
-                    {/* <div
-                        tabIndex={0}
-                        className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
-                        <div className="card-body">
-                            <span className="text-lg font-bold">8 Items</span>
-                            <span className="text-info">Subtotal: $999</span>
-                            <div className="card-actions">
-                                <button className="btn btn-primary btn-block">View cart</button>
-                            </div>
-                        </div>
-                    </div> */}
                 </div>
                 <div className="dropdown dropdown-end">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
                             <img
-                                alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                src={user?.photoURL? user?.photoURL : "https://i.ibb.co.com/3z773GB/avatar.png"} />
                         </div>
                     </div>
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
                         <li>
-                           <p className="">{user? user?.name ? user.name : user?.email : ""}</p>
+                           <p className="">{user? user?.displayName : ""}</p>
                         </li>
                         <li>{user? <Link to="">Dashboard</Link> : <Link to="/login">Login</Link>}</li>
                         <li>{user? <p onClick={handleLogOut}>Logout</p> : <Link to="/register">Register</Link>}</li>
